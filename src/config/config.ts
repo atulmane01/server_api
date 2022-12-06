ecf01a91f378342942bc18d06a996af4bf8a4fbe
atul_mane <atul.mane@tibilsolution.com>
2022-12-06 22:39:10 +0530

    commit

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+// import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+// import { TaskEntity } from "src/tasks/entity/task.entity";
+// export const typeOrmConfig : TypeOrmModuleOptions ={
+//     type:'postgres',
+//     host:'localhost',
+//     username:'postgres',
+//     password:'postgres',
+//     database:'postgres',
+//     port:5432,
+//     entities:[TaskEntity],
+//     synchronize:true
+// }
